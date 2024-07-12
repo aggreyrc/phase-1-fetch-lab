98ba8af75a93808eef888932757ddda163a05c55
aggreyrc <aggreyronoh@gmail.com>
2024-07-13 00:49:29 +0300

    fetch-lab

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+      renderBooks(object);
+    })
+    .catch(function(error) {
+      alert("Books Unavailable");
+      console.log(error);
+    });
 }
 
 function renderBooks(books) {
